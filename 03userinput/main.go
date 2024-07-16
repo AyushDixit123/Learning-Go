d75c31c2685c99894ef8cfe67f88f4a625a5c2e9
AyushDixit123 <132762104+AyushDixit123@users.noreply.github.com>
2024-07-16 16:37:15 +0530

    time

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -16,6 +16,6 @@ func main() {
 	// whatever the reader reads is stored in comma ok syntax variable
 	// comma ok || err err
 
-	input, _ := reader.ReadString('\n')
+	input, _ := reader.ReadString('\n') //it shows it will keep reading until next line ius hit
 	fmt.Println("thanks for rating,", input)
 }
