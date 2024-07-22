9c583c5ec17eaca3fcc4925371677263b676cd0a
AyushDixit123 <ayushdixitworkmail@gmail.com>
2024-07-22 16:03:42 +0530

    slices

--- 09slices/main.go
+++ 09slices/main.go
@@ -5,6 +5,7 @@ import (
 	"sort"
 )
 
+// Important
 func main() {
 	var fruitList = []string{"apple", "tomato"}
 	fmt.Printf("type of fruitlist is %T", fruitList)
@@ -19,8 +20,8 @@ func main() {
 	// it will show data of collections after the index mentioned in collection
 	fruitList = append(fruitList[1:])
 	fmt.Println(fruitList)
-	fruitList = append(fruitList[1:3]) //starts mfrom position 1 to 3
-
+	fruitList = append(fruitList[:2]) //starts mfrom position 1 to 3
+	fmt.Println(fruitList)
 	highScores := make([]int, 2) // we are creating a space in memory for this variable that is a slices with size 4
 	highScores[0] = 123
 	highScores[1] = 345
@@ -29,4 +30,12 @@ func main() {
 	// this will not cause an error
 	//append reallocates the memory and data is allocated, increasing performance
 	sort.Ints(highScores) //sorts int slices in increasing order
+
+	//how to remove a value from slice based on index
+	var courses = []string{"react", "js", "swift", "python", "ruby"}
+
+	index := 2
+
+	courses = append(courses[:index], courses[index+1:]...)
+	fmt.Println(courses)
 }
